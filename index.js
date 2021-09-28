8156c84a2b121de36cff8a423a56035eddc71829
bondrnk <hanna.bondarenko.s@gmail.com>
2021-09-28 23:01:07 +0300

    Added loading page

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Loading page
+
+setTimeout(() => {
+  document.getElementById("page").style.display = "block";
+  document.getElementById("loading").style.display = "none";
+}, 6000)
+
 // Burger menu
 
 const burgerMenu = document.querySelector(".e-burger-menu");
